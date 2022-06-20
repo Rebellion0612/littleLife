cffa0c2aacbc0ed6dbd233552bb048fc9aec23bc
zhangpengfei <790646988@qq.com>
2022-06-20 23:39:20 +0800

    init

---
+++ src/main/java/com/phoenix/littlelife/LittleLifeApplication.java
@@ -0,0 +1,13 @@
+package com.phoenix.littlelife;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LittleLifeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LittleLifeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/phoenix/littlelife/LittleLifeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phoenix.littlelife;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LittleLifeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
